67cc4e1e780a57c255a926d77d6249691b841946
JdotCarver <jak21@hotmail.fr>
2025-04-21 15:52:33 +0200

    Create Known_Situations.lua

---
+++ Test Version/Known_Situations.lua
@@ -0,0 +1,23 @@
+-- ✅ Confirmed known situations
+local known_situations = {
+    [1]  = "Online",
+    [2]  = "SoloOnline",
+    [3]  = "Offline",
+    [4]  = "QuestPreparing",
+    [5]  = "QuestReady",
+    [6]  = "QuestPlayingHost",
+    [7]  = "QuestPlayingGuest",
+    [16] = "PressSelectToEndQuest (QuestHagitori)",
+    [17] = "QuestEndAnnounce",
+    [18] = "QuestResultScreen",
+    [29] = "QuestLoadingResultScreen (MissionLv2)",
+    [36] = "TrainingArea",
+    [37] = "JunctionArea",
+    [38] = "Suja (ST503)",
+    [39] = "GrandHall (ST404)",
+    [40] = "TrainingArea",
+    [41] = "PlayingBowlingGame",
+    [42] = "PlayingArmWrestling",
+    [43] = "TableAction",
+    [44] = "SeemsAlwaysOn"
+}
